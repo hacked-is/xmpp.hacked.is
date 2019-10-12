98430290187445d9a9342a02c1bc1c5dc5a7a266
Beard <beardlyness@users.noreply.github.com>
2019-10-12 01:49:14 -0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,52 +6,52 @@ PROSODY_DATA_DIR="/var/lib/prosody"
 ROOT_SCRIPTS_DIR="/root/scripts"
 
 # Run chown-root.sh to force root ownership
-bash "${GIT_DIR}"/xmpp.is/scripts/chown-root.sh
+bash "${GIT_DIR}"/hacked.is/scripts/chown-root.sh
 
 # Prosody config
 echo
 echo "Syncing Prosody configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/prosody/ /etc/prosody/
+rsync -av "${GIT_DIR}"/hacked.is/etc/prosody/ /etc/prosody/
 
 # Hiawatha config
 echo
 echo "Syncing Hiawatha configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/hiawatha/ /etc/hiawatha/
+rsync -av "${GIT_DIR}"/hacked.is/etc/hiawatha/ /etc/hiawatha/
 
 # Dehydrated config
 echo
 echo "Syncing Dehydrated configs"
-rsync -av "${GIT_DIR}"/xmpp.is/home/user/git/dehydrated/ "${GIT_DIR}"/dehydrated/
+rsync -av "${GIT_DIR}"/hacked.is/home/user/git/dehydrated/ "${GIT_DIR}"/dehydrated/
 
 # Tor config
 echo
 echo "Syncing Tor configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/tor/ /etc/tor/
+rsync -av "${GIT_DIR}"/hacked.is/etc/tor/ /etc/tor/
 
 # Apt config
 echo
 echo "Syncing Apt configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/apt/ /etc/apt/
+rsync -av "${GIT_DIR}"/hacked.is/etc/apt/ /etc/apt/
 
 # Logrotate
 echo
 echo "Syncing logrotate configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/logrotate.d/ /etc/logrotate.d/
+rsync -av "${GIT_DIR}"/hacked.is/etc/logrotate.d/ /etc/logrotate.d/
 
 # OpenSSL
 echo
 echo "Syncing OpenSSL configs"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/ssl/ /etc/ssl/
+rsync -av "${GIT_DIR}"/hacked.is/etc/ssl/ /etc/ssl/
 
 # Webroot
 echo
 echo "Syncing /var/www/"
-rsync -av "${GIT_DIR}"/xmpp.is/var/www/ /var/www/
+rsync -av "${GIT_DIR}"/hacked.is/var/www/ /var/www/
 
 # Mercurial
 echo
 echo "Syncing /etc/mercurial"
-rsync -av "${GIT_DIR}"/xmpp.is/etc/mercurial/ /etc/mercurial/
+rsync -av "${GIT_DIR}"/hacked.is/etc/mercurial/ /etc/mercurial/
 
 # Prosody Modules
 
@@ -68,5 +68,5 @@ rsync -av "${GIT_DIR}"/mod_register_web/ /var/lib/prosody/modules/mod_register_w
 # Cron
 echo
 echo "Syncing crontabs"
-crontab -u root "${GIT_DIR}"/xmpp.is/var/spool/cron/crontabs/root
-crontab -u user "${GIT_DIR}"/xmpp.is/var/spool/cron/crontabs/user
+crontab -u root "${GIT_DIR}"/hacked.is/var/spool/cron/crontabs/root
+crontab -u user "${GIT_DIR}"/hacked.is/var/spool/cron/crontabs/user
